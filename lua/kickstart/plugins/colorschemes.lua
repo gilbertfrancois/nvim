29a11905bc6e50dcc7bb3beb825f83d2e278a588
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2025-04-02 10:03:22 +0200

    Small updates

--- lua/kickstart/plugins/colorschemes.lua
+++ lua/kickstart/plugins/colorschemes.lua
@@ -1,11 +1,5 @@
 return {
     {
-        'RRethy/base16-nvim',
-        -- init = function()
-        --     vim.cmd.colorscheme 'base16-onedark'
-        -- end,
-    },
-    {
         'navarasu/onedark.nvim',
         priority = 1000,
         opts = {
@@ -19,17 +13,5 @@ return {
             vim.cmd.colorscheme 'onedark'
             vim.cmd.hi 'Comment gui=none'
         end,
-    },
-    {
-        "rose-pine/neovim",
-        name = "rose-pine",
-        config = function()
-            require('rose-pine').setup({
-                disable_background = true,
-                styles = {
-                    italic = false,
-                },
-            })
-        end
     },
 }

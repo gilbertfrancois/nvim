517b9ea8c0c3588f08ffef5d0a87a158cb286171
Gilbert Francois Duivesteijn <gilbert@deep-impact.ch>
2025-07-07 14:51:30 +0200

    better clip board

--- lua/options.lua
+++ lua/options.lua
@@ -39,9 +39,11 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.schedule(function()
-    vim.opt.clipboard = 'unnamedplus'
-end)
+-- vim.schedule(function()
+-- vim.opt.clipboard = 'unnamedplus'
+vim.g.clipboard = 'osc52'
+vim.opt.clipboard:append { 'unnamed', 'unnamedplus' } -- use + register by default
+-- end)
 
 -- Enable break indent
 vim.opt.breakindent = true
